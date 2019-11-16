f339a4c5f7985914cd2fb81a98c79b47d4b6798e
JKWrobel <bociek428@gmail.com>
2019-11-16 21:40:26 +0100

    Added grounwork for build ViewModel

--- Project/ViewModel/ViewModelBase.cs
+++ Project/ViewModel/ViewModelBase.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ViewModel
 {
-    class ViewModelBase
+    class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }

c246b6a17799ef4bdb9dc0d8c7eb6fc79464c586
JKWrobel <bociek428@gmail.com>
2019-11-16 21:30:34 +0100

    Added placeholder type generator

---
+++ Project/Model/PlaceholderForTypeManagement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class PlaceholderForTypeManagement
+    {
+        List<TypePlaceholder> PlaceholderGetTypesForType(TypePlaceholder typePlaceholder)
+        {
+            List<TypePlaceholder> typePlaceholders = new List<TypePlaceholder>();
+            int numberOfChildren = new Random().Next(1, 4);
+            for (int i = 0; i < numberOfChildren; i++)
+            {
+                TypePlaceholder tempType = new TypePlaceholder()
+                {
+                    Name = "TempName" + i
+                };
+
+                typePlaceholders.Add(tempType);
+            }
+
+            return typePlaceholders;
+        }
+    }
+}
---
+++ Project/Model/TypePlaceHolder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Model
+{
+    public class TypePlaceholder
+    {
+        public string Name
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(_name))
+                {
+                    return _name;
+                }
+                else
+                {
+                    return "ValueNotSet";
+                }
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                _name = value;
+            } 
+        }
+
+        public ObservableCollection<TypePlaceholder> ReeferencedTypes
+        {
+            get
+            {
+                if (_referencedTypes != null)
+                {
+                    return _referencedTypes;
+                }
+                else
+                {
+                    return "ValueNotSet";
+                }
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                _name = value;
+            }
+        }
+
+        private ObservableCollection<TypePlaceholder> _referencedTypes = null;
+        private string _name;
+    }
+}

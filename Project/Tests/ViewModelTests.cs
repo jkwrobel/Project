3787d4a8a1a0e19d26dfcc3fbb5004c49cf6f50f
JKWrobel <bociek428@gmail.com>
2019-11-20 12:52:29 +0100

    Implemented Some ViewModel tests

--- Project/Tests/ViewModelTests.cs
+++ Project/Tests/ViewModelTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
 
 namespace ViewModel.Tests
 {
@@ -10,7 +11,7 @@ namespace ViewModel.Tests
     public class ViewModelBaseTests
     {
         [TestMethod()]
-        public void TreeViewTypeElementTest()
+        public void ViewModelBaseTest()
         {
             ViewModelBaseTestClass testClass = new ViewModelBaseTestClass();
             bool hasBeenRaised = false;
@@ -38,17 +39,46 @@ namespace ViewModel.Tests
     public class TreeViewViewModelTests
     {
         [TestMethod()]
-        public void TreeViewTypeElementTest()
+        public void TreeViewViewModelTest()
         {
             TreeViewViewModel treeViewViewModel = new TreeViewViewModel();
             Assert.AreEqual(new ObservableCollection<TreeViewTypeElement>().GetType(),
             treeViewViewModel.ReferencedTypes.GetType());
+            Assert.IsFalse(treeViewViewModel.HasTypeManager);
+            treeViewViewModel.TypeManagerInst = new TypeManager();
+            Assert.IsTrue(treeViewViewModel.ReferencedTypes.Count == 0);
+            treeViewViewModel.ShowTreeViewCommand.Execute(this);
             Assert.IsTrue(treeViewViewModel.ReferencedTypes.Count > 0);
         }
     }
 
     [TestClass()]
     public class TreeViewTypeElementTests
+    {
+        [TestMethod()]
+        public void TreeViewTypeElementTest()
+        {
+            ITypeManager typeManager = new TypeManager();
+            ATypeRepresentation typeRepresentation = new TypePlaceholder(){ Name = "First" };
+            TreeViewTypeElement treeViewTypeElement = new TreeViewTypeElement(typeManager, typeRepresentation);
+            Assert.AreEqual("First", treeViewTypeElement.Name);
+            Assert.IsNotNull(treeViewTypeElement.ReferencedTypes);
+            Assert.IsTrue(treeViewTypeElement.ReferencedTypes.Count > 0);
+
+        }
+    }
+    [TestClass()]
+    public class ShowTreeViewCommandTests
+    {
+        [TestMethod()]
+        public void TreeViewTypeElementTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestClass()]
+    public class AssignTypeManagerCommandTests
     {
         [TestMethod()]
         public void TreeViewTypeElementTest()
